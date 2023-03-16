533b6a83e24fbed8f9b94ab7543acb27249f0956
residentevil0803 <4081040+residentevil0803@users.noreply.github.com>
2023-03-17 06:32:27 +0800

    Fix header

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,35 +1,23 @@
 <script>
-  import LogoWhite from '../assets/imgs/logoWhite.png'
+  import LogoImg from '../assets/imgs/logoWhite.png'
+  import TwitterImg from '../assets/imgs/twitter.svg'
+  import DiscordImg from '../assets/imgs/discord.svg'
 
   export let route;
 </script>
 
 <main>
-  <div class="absolute top-0 left-0 w-full flex flex-row px-0 md:px-40 items-center h-28">
-    <div class="absolute opacity-50 bg-black top-0 left-0 w-full h-full z-10"></div>
-
-
-    <a href="/" class="cursor-pointer flex items-center gap-2 justify-center md:justify-start grow relative z-20 text-white">
-      <img src={LogoWhite} class="w-auto h-12" alt="logoWhite">
-      <h1 class="text-4xl leading-none">Satoshi Punks</h1>
-    </a>
-
-    <ul class="hidden md:flex text-4xl items-center relative z-20 text-white gap-8">
-      <li>
-        <a href={route == "home" ? "#Manifesto" : "/#Manifesto"}>Manifesto</a>
-      </li>
-      <li>
-        <a href="/gallery">Gallery</a>
-      </li>
-      <li>
-        <a href={route == "home" ? "#Gang" : "/#Gang"}>Gang</a>
-      </li>
-      <li>
-        <a href="https://discord.gg/satoshipunks">Buy a Punk</a>
-      </li>
-      <li>
-       
-      </li>
-    </ul>
+  <div class="absolute top-0 w-full flex justify-center h-20 backdrop-blur-md bg-[rgba(0,0,0,0.3)] z-10">
+    <div class="w-full max-w-[1440px] flex px-[112px] justify-between items-center">
+        <a href="/"> <img src={LogoImg} class="w-[254px] h-8" alt=""></a>
+        <div class="flex gap-8 items-center text-[32px] leading-[44px] font-normal tracking-[-0.02px] text-white">
+          <a href={route == "home" ? "#Manifesto" : "/#Manifesto"} class="px-2">Manifesto</a>
+          <a href="/gallery">Gallery</a>
+          <a href={route == "home" ? "#Gang" : "/#Gang"}>Team</a>
+          <a href={"/buy"}>Buy</a>
+          <a href="https://tiwtter.com"><img src={TwitterImg} class="w-6 h-6" alt=""></a>
+          <a href="https://discord.gg/satoshipunks" class="w-11 h-11 flex items-center justify-center"><img src={DiscordImg} class="w-6 h-6" alt=""></a>
+        </div>
+    </div>
   </div>
 </main>
